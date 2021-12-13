481c46c77f5e2bca01351e5645910061addaee1f
a.pirogov <a.pirogov@fz-juelich.de>
2021-12-13 12:49:07 +0100

    Make installation with pip work, closes #5

--- metador/__init__.py
+++ metador/__init__.py
@@ -1,22 +1,19 @@
 """Some useful helpers to access files shipped with package."""
 
-import os
 import sys
 from pathlib import Path
 
 import toml
 from typing_extensions import Final
 
-__pkg_path__: Final[Path] = Path(
-    os.path.abspath(os.path.dirname(sys.modules[__name__].__file__))
-)
+__pkg_path__: Final[Path] = Path(sys.modules[__name__].__file__).parent.resolve()
 
 # get root path of project (above the module dir)
-__basepath__: Final[Path] = Path(os.path.abspath(os.path.dirname(__pkg_path__)))
+__basepath__: Final[Path] = __pkg_path__.parent.resolve()
 
 
 # single source of truth for version is the pyproject.toml!
-pyproject = toml.load(os.path.join(__basepath__, "pyproject.toml"))
+pyproject = toml.load(__basepath__ / "pyproject.toml")
 
 __version__: Final[str] = pyproject["tool"]["poetry"]["version"]
 
